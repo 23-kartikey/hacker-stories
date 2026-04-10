96e6b86b3fd15728bc3da8d8f5c5beed84e6de1e
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-04-10 12:37:27 +0530

    feat: created App and List components

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -1,11 +1,44 @@
-const title='react';
+const list=[
+    {
+      title: 'React',
+      url: 'https://react.dev/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.dev/',
+      author: 'Kevin Hart',
+      num_comments: 2,
+      points: 5,
+      objectID: 1
+    }
+  ]
 
 function App() {
-
   return (
     <div>
-      <h1>Hello {title}</h1>
+      <h1>My Hacker Stories</h1>
+      <label htmlFor="search">Search: </label>
+      <input type="text" id="search" />
+      <hr />
+      <List />
     </div>
+  );
+}
+
+function List(){
+  return(
+    <li>{list.map(item=>(
+      <ul key={item.objectID}>
+        <span><a href={item.url}>{item.title}</a></span>
+        <span> {item.author} </span>
+        <span> {item.points} </span>
+        <span> {item.num_comments} </span>
+      </ul>
+    ))}</li>
   );
 }
 
