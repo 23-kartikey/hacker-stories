43636ad6cbed1043cb4a550f35599b39698ff6ea
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-05-09 00:07:45 +0530

    refactor: pratice end

--- hacker-stories/src/App.tsx
+++ hacker-stories/src/App.tsx
@@ -1,148 +1,110 @@
-// import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
-// type Story = {
-//   title: string;
-//   url: string;
-//   author: string;
-//   num_comments: number;
-//   points: number;
-//   objectID: number;
-// }
+type Story = {
+  title: string;
+  url: string;
+  author: string;
+  num_comments: number;
+  points: number;
+  objectID: number;
+}
 
-// const useStorageItem=(key: string, initialState: string)=>{
-//   const [value, setValue]=useState(localStorage.getItem(key)||initialState);
+const useStorageItem=(key: string, initialState: string)=>{
+  const [value, setValue]=useState(localStorage.getItem(key)||initialState);
 
-//   useEffect(()=>{
-//     localStorage.setItem(key, value);
-//   },[key, value]);
+  useEffect(()=>{
+    localStorage.setItem(key, value);
+  },[key, value]);
 
-//   return [value, setValue] as const;
-// }
+  return [value, setValue] as const;
+}
 
-// const App=()=>{
+const App=()=>{
 
-//   const stories : Story[] = [
-//     {
-//       title: 'React',
-//       url: 'https://react.dev/',
-//       author: 'Jordan Walke',
-//       num_comments: 3,
-//       points: 4,
-//       objectID: 0
-//     },
-//     {
-//       title: 'Redux',
-//       url: 'https://redux.dev/',
-//       author: 'Kevin Hart',
-//       num_comments: 2,
-//       points: 5,
-//       objectID: 1
-//     }
-//   ]
+  const stories : Story[] = [
+    {
+      title: 'React',
+      url: 'https://react.dev/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.dev/',
+      author: 'Kevin Hart',
+      num_comments: 2,
+      points: 5,
+      objectID: 1
+    }
+  ]
 
-//   const [searchTerm, setSearchTerm]=useStorageItem('search', 'React');
+  const [searchTerm, setSearchTerm]=useStorageItem('search', 'React');
 
-//   const handleSearch=(event: React.ChangeEvent<HTMLInputElement>)=>{
-//     setSearchTerm(event.target.value);
-//   }
+  const handleSearch=(event: React.ChangeEvent<HTMLInputElement>)=>{
+    setSearchTerm(event.target.value);
+  }
 
 
-//   return(
-//     <div>
-//       <h1>My Hacker Stories</h1>
-//       <InputWithLabel isFocused id="search" value={searchTerm} type="text" handleInput={handleSearch}><strong>Search: </strong></InputWithLabel>
-//       <p>Searching for: {searchTerm}</p>
-//       <hr />
-//       <List list={stories.filter(story=>story.title.toLowerCase().includes(searchTerm.toLowerCase()))} />
-
-//     </div>
-//   );
-// }
-
-// type ListProps={
-//   list: Story[];
-// }
-
-// const List=({list}: ListProps)=>(
-//     <ul>{list.map(({objectID, ...item})=>(
-//       <Item  key={objectID} {...item} />
-//     ))}</ul>
-//   );
-
-//   type ItemProps={
-//     title: string;
-//     url:string;
-//     author: string;
-//     points: number;
-//     num_comments: number;
-//   }
-
-//   const Item=({title, url, author, points, num_comments}: ItemProps)=>{
-//     return(
-//       <li>
-//         <span><a href={url}>{title}</a></span>
-//         <span> {author} </span>
-//         <span> {points} </span>
-//         <span> {num_comments} </span>
-//       </li>
-//     );
-//   }
-
-// type InputWithLabelProps={
-//     isFocused: boolean,
-//     id:string,
-//     value: string,
-//     type?: string,
-//     handleInput: (event: React.ChangeEvent<HTMLInputElement>) => void,
-//     children: React.ReactNode;
-//   }
-
-// const InputWithLabel=({ isFocused, id, value, type="text", handleInput, children}: InputWithLabelProps)=>{
-//   let name="";
-  
-//   return (
-//     <>
-//       <label htmlFor={id}>{children}</label>
-//       &nbsp;
-//       <input autoFocus={isFocused} type={type} id={id} value={value} onChange={handleInput} />
-//     </>
-//   );
-// }
-
-// export default App;
-
-import { useState, useEffect, useRef } from 'react';
-
-function App(){
   return(
-    <Kartikey />
+    <div>
+      <h1>My Hacker Stories</h1>
+      <InputWithLabel isFocused id="search" value={searchTerm} type="text" handleInput={handleSearch}><strong>Search: </strong></InputWithLabel>
+      <p>Searching for: {searchTerm}</p>
+      <hr />
+      <List list={stories.filter(story=>story.title.toLowerCase().includes(searchTerm.toLowerCase()))} />
+
+    </div>
   );
 }
 
-function Kartikey(){
+type ListProps={
+  list: Story[];
+}
 
-  const [text, setText]=useState("Some text here...");
-  const [size, setSize]=useState({width:0, height:0});
+const List=({list}: ListProps)=>(
+    <ul>{list.map(({objectID, ...item})=>(
+      <Item  key={objectID} {...item} />
+    ))}</ul>
+  );
 
-  const handleChange=(e: React.ChangeEvent<HTMLInputElement>)=>{
-    setText(e.target.value);
+  type ItemProps={
+    title: string;
+    url:string;
+    author: string;
+    points: number;
+    num_comments: number;
   }
 
-  const ref=(node: HTMLSpanElement)=>{
-    if(!node) return;
-    console.log("Kartikey")
-    const {width, height} = node.getBoundingClientRect();
-    setSize({width, height});
-
+  const Item=({title, url, author, points, num_comments}: ItemProps)=>{
+    return(
+      <li>
+        <span><a href={url}>{title}</a></span>
+        <span> {author} </span>
+        <span> {points} </span>
+        <span> {num_comments} </span>
+      </li>
+    );
   }
 
+type InputWithLabelProps={
+    isFocused: boolean,
+    id:string,
+    value: string,
+    type?: string,
+    handleInput: (event: React.ChangeEvent<HTMLInputElement>) => void,
+    children: React.ReactNode;
+  }
 
-  return(
+const InputWithLabel=({ isFocused, id, value, type="text", handleInput, children}: InputWithLabelProps)=>{
+  let name="";
+  
+  return (
     <>
-    <input type="text" value={text} onChange={handleChange} />
-    <br/>
-    <span ref={ref}>{text}</span><br/>
-    <p>Here are the height and width: <br/> Height: {size.height} <br/> Width: {size.width}</p>
+      <label htmlFor={id}>{children}</label>
+      &nbsp;
+      <input autoFocus={isFocused} type={type} id={id} value={value} onChange={handleInput} />
     </>
   );
 }
